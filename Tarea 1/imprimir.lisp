b8419ae1ae8d06b053d949a88dbef74740518cef
Víctor Cruz <vcruzgon@itam.mx>
2018-02-24 08:55:33 -0600

    Se comentó "imprimir.lisp" y se creó un documento con el mismo código
    A LISP no le gustó que comentara entre líneas, así que el documento no tiene comentarios para que se vaya de corrido y compile correctamente

--- Tarea 1/imprimir.lisp
+++ Tarea 1/imprimir.lisp
@@ -1,25 +1,28 @@
 (Defun imprime(lst)
-(setq abuelos nil padres nil hijos nil)
+(setq abuelos nil padres nil hijos nil) ; Quiero que funcionene como una cola
 (push '_ hijos)
 (getLvl lst)
-(print (reverse abuelos))
-(print 'LISTO))
+(reverse abuelos)) ;Para imprimir la ultima iteracion que se dejo porque el
+  ; algoritmo se salio antes
 
 (Defun getLvl(lst)
-(loop
+(loop ; Iteramos toda la lst y vamos elemento a elemento
+  ; Al terminar con la lista que se esta iterando:
   (when (null lst)
-    (if (eq (car (last hijos)) '_)
+    (if (eq (car (last hijos)) '_) ; Lo que nos ayuda a ver a todos los hijos
+    ; de todos los padres están "agrupados" en Hijos
       (let ((hijosTmp (reverse hijos)) )
-        (pop hijosTmp)
-        (setq hijos (reverse hijosTmp))
-        (if (not (null hijos))
+        (pop hijosTmp) ; Quitamos el "_" de la cola
+        (setq hijos (reverse hijosTmp)) ; Actualizamos Hijos sin ese "_"
+        (if (not (null hijos)) ; Para que no se quede en LOOP
           (push '_ hijos))
-        (if (not (null abuelos))
+        (if (not (null abuelos)) ; Para que no imprima a lo menso NIL
           (print (reverse abuelos)))
+        ; Pasamos Padres->Abuelos
         (setq abuelos padres)
         (setq padres nil)))
-    (return 8))
-  (setq elem (pop lst))
+    (return nil)) ; El 8 es solo un valor x
+  (setq elem (pop lst)) ; Vamos al nuevo elemento de la lista y lo borramos de ella
   (cond ((null elem) )
     ((atom elem) (push elem padres))
     (t (push elem hijos))))
