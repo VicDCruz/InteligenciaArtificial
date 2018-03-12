b0705384e649c1aa0b163f5a8a950dfb573addbc
Víctor Cruz <vcruzgon@itam.mx>
2018-03-12 17:35:05 -0600

    Se acabo de escribir el código de rbfs y se puso otro ejemplo más complicado

--- Tarea2/Lisp/aStarSearch.lisp
+++ Tarea2/Lisp/aStarSearch.lisp
@@ -1,17 +1,19 @@
 ; DUMMY!!!
 (defun inicio(dummy)
-  (setq ciudades '(A B C D)
-      costos '(0 7 10 15) ; Path Cost [h(n)]
+  (setq ciudades '(A B C D E F)
+      costos '(0 8 35 92 61 8) ; Path Cost [h(n)]
       distancias '( ; Step Cost [g(n)]
-                  (0 0 1 0) ; Para A
-                  (0 0 7 10) ; Para B
-                  (1 7 0 6) ; Para C
-                  (0 10 6 0) ; Para D
+                  (0 1 0 4 0 0) ; Para A
+                  (1 0 5 7 3 6) ; Para B
+                  (0 5 0 0 0 9) ; Para C
+                  (4 7 0 0 8 0) ; Para D
+                  (0 3 0 8 0 2) ; Para E
+                  (0 6 9 0 2 0) ; Para F
                   )
       num 2
       recorrido '()))
 
-; Estructura del NODO: (# Padre Nom f(n))
+; Estructura del NODO: (# Padre Nom f(n) NomPadre)
 ; f(n) = g(n) + h(n)
 
 ; Regresa un nodo de una ciudad con su posición, su nombre y su costo, dado un NOMBRE
@@ -31,12 +33,14 @@
 
 ; Va a la matriz de distancias, obtiene la tupla de CIUDAD y obtiene el nombre
 ; de la ciudad que tiene una distancia diferente de 0
-(defun obtenSucesores(ciudad)
+(defun obtenSucesores(ciudad padre)
   (setq infoC (buscaCiudad ciudad) vecinos '() distVecinos (nth (- (car infoC) 1) distancias) cont 1)
-  (mapcar #'(lambda (elem)
-    (if (not (eq elem 0))
-      (push (nth (- cont 1) ciudades) vecinos))
-    (incf cont)) distVecinos)
+  (let ((contVecino 1))
+    (mapcar #'(lambda (elem)
+      (if (and (not (eq elem 0)) (not (eql padre (nth (- cont 1) ciudades))))
+        (push (nth (- cont 1) ciudades) vecinos))
+      (incf cont)
+      (incf contVecino)) distVecinos))
   (reverse vecinos))
 
 (defun calcF(ciudad destino)
@@ -51,7 +55,7 @@
 
 (defun segundoMin(lst primerMin)
   (let (minimo '())
-    (setq minimo (car lst))
+    (setq minimo (if (not (eq (fourth (car lst)) (fourth primerMin))) (car lst) (cadr lst)))
     (mapcar #'(lambda (elem)
       (setq minimo (if (and (< (fourth elem) (fourth minimo)) (> (fourth elem) (fourth primerMin))) elem minimo))) lst)
   minimo))
@@ -60,17 +64,20 @@
 (defun mejorRuta(ini fin)
   (inicio nil)
   (setq contPasos 0)
-  (rbfs fin (list 1 0 ini (calcF ini fin)) nil))
+  (rbfs fin (list 1 0 ini (calcF ini fin) nil) nil)
+  recorrido)
+  ; Recordar que el nodo inicial esta (# padre Nom f(n) NomPadre)
 
 (defun rbfs(fin nodo fLimit)
-  (when (eq contPasos 20) (return-from rbfs 'Nada))
-  (when (eq fin (third nodo)) (push nodo recorrido) (return-from rbfs (list (third nodo))))
+  (incf contPasos)
+  (when (eq contPasos 50) (return-from rbfs 'Nada))
+  (when (eq fin (third nodo)) (return-from rbfs (list (third nodo))))
   (let (sucesores '())
     (let (hijos '())
-      (setq hijos (obtenSucesores (third nodo)))
+      (setq hijos (obtenSucesores (third nodo) (fifth nodo)))
       (print hijos)
       (mapcar #'(lambda (elem)
-        (push (list num (car nodo) elem nil) sucesores) ; f(n) se pone después
+        (push (list num (car nodo) elem nil (third nodo)) sucesores) ; f(n) se pone después
         (incf num)) hijos))
     (print sucesores)
     (when (null sucesores) (return-from rbfs (list 'Fallo nil)))
@@ -79,9 +86,12 @@
     (print sucesores)
     (loop
       (setq mejor (minimum sucesores))
+      (print 'mejor)
+      (print mejor)
       (when (> (fourth mejor) (if (null fLimit) (+ 1 (fourth mejor)) fLimit)) (return (list 'Fallo (fourth mejor))))
       (setq alternativa (segundoMin sucesores mejor))
+      (print 'alternativa)
       (print alternativa)
       (setq resRBFS (rbfs fin mejor (min (if (null fLimit) (+ 1 (fourth alternativa)) fLimit) (fourth alternativa))) resultado (car resRBFS))
       (setf (fourth mejor) (second resRBFS))
-      (if (not (eql resultado 'Fallo)) (return-from rbfs resultado)))))
+      (when (not (eql resultado 'Fallo)) (push resultado recorrido) (return-from rbfs (list (third nodo))))))))
